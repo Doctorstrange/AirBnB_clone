5a899b21a0e935bcce4f35e33c4fc938d5b05812
Doctorstrange <Doctorstrange>
2023-10-15 20:56:21 -0500

    Write a class User that inherits from BaseModel:

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -33,12 +33,6 @@ class TestFileStorage_instantiation(unittest.TestCase):
     def test_FileStorage_file_path_is_private_str(self):
         self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
-    def testFileStorage_objects_is_private_dict(self):
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-
-    def test_storage_initializes(self):
-        self.assertEqual(type(models.storage), FileStorage)
-
 
 class TestFileStorage_methods(unittest.TestCase):
     """Unittests for testing methods of the FileStorage class."""
