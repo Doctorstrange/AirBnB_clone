550b0f2d322a7bc2337ba3954d20af9323b12a41
Doctorstrange <Doctorstrange>
2023-10-15 21:17:51 -0500

    Write a class User that inherits from BaseModel:

--- tests/test_console.py
+++ tests/test_console.py
@@ -62,30 +62,5 @@ class TestHBNBCommand_create(unittest.TestCase):
             self.assertEqual(ex, output.getvalue().strip())
 
 
-class TestAmenity_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Amenity class."""
-
-    def test_no_args_instantiates(self):
-        self.assertEqual(Amenity, type(Amenity()))
-
-    def test_new_instance_stored_in_objects(self):
-        self.assertIn(Amenity(), models.storage.all().values())
-
-    def test_id_is_public_str(self):
-        self.assertEqual(str, type(Amenity().id))
-
-    def test_created_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(Amenity().created_at))
-
-    def test_updated_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(Amenity().updated_at))
-
-    def test_name_is_public_class_attribute(self):
-        am = Amenity()
-        self.assertEqual(str, type(Amenity.name))
-        self.assertIn("name", dir(Amenity()))
-        self.assertNotIn("name", am.__dict__)
-
-
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines unittests for console.py."""
+
+
+import os
+import sys
+import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+from models.amenity import Amenity
+import models
+from datetime import datetime
+
+class TestAmenity_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Amenity class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(Amenity, type(Amenity()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Amenity(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(Amenity().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Amenity().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Amenity().updated_at))
+
+    def test_name_is_public_class_attribute(self):
+        am = Amenity()
+        self.assertEqual(str, type(Amenity.name))
+        self.assertIn("name", dir(Amenity()))
+        self.assertNotIn("name", am.__dict__)
+
+
+if __name__ == "__main__":
+    unittest.main()
