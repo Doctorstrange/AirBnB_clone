01e0dbe100c117cc48c64264412be0c78aa3c521
Doctorstrange <Doctorstrange>
2023-10-15 20:47:03 -0500

    Write a class User that inherits from BaseModel:

--- tests/test_console.py
+++ tests/test_console.py
@@ -33,5 +33,31 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("help quit"))
             self.assertEqual(ask, output.getvalue().strip())
 
+    class TestHBNBCommand_create(unittest.TestCase):
+    """test create command"""
+
+    def test_create_missing_class(self):
+        ex = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual(ex, output.getvalue().strip())
+
+    def test_create_invalid_class(self):
+        ex = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create MyModel"))
+            self.assertEqual(ex, output.getvalue().strip())
+
+    def test_create_invalid_syntax(self):
+        ex = "*** Unknown syntax: MyModel.create()"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("MyModel.create()"))
+            self.assertEqual(ex, output.getvalue().strip())
+        ex = "*** Unknown syntax: BaseModel.create()"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("BaseModel.create()"))
+            self.assertEqual(ex, output.getvalue().strip())
+
+
 if __name__ == "__main__":
     unittest.main()
